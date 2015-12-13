e9ab5a5fec1a9494fc072b5f54e524d60be3427c
Vitaliy Krutko <asmxforce@gmail.com>
2015-12-13 20:57:24 +0200

    Disabled auto-configuration

--- src/main/java/com/asmx/Application.java
+++ src/main/java/com/asmx/Application.java
@@ -1,36 +1,15 @@
 package com.asmx;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
-
-import javax.sql.DataSource;
 
 @Configuration
-@EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
-    @Value("${datasource.jndi-name}")
-    protected String datasourceJndiName;
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        if (StringUtils.isBlank(datasourceJndiName)) {
-            return null;
-        }
-
-        JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
-        return dataSourceLookup.getDataSource(datasourceJndiName);
     }
 
     public static void main(String[] args) throws Exception {
